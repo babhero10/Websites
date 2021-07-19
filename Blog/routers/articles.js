be4c569fe083708f8e0d7f04a574b09b712e2983
babhero10 <abdallah.elsaid25@gmail.com>
2021-07-20 00:16:16 +0200

    Set routes in articles

---
+++ Blog/routers/articles.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/all', (req, res) => {
+    
+});
+
+router.get('/add', (req, res) => {
+
+});
+
+router.post('/add', (req, res) => {
+
+});
+
+module.exports = router;
