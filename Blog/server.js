21cd276cf82ec8b1c1e96a1bbbfe10be0e4b4bf4
babhero10 <abdallah.elsaid25@gmail.com>
2021-07-20 23:56:57 +0200

    Add layout

--- Blog/server.js
+++ Blog/server.js
@@ -4,14 +4,16 @@ const port = 5000;
 /* Set express app */
 const express = require('express');
 const app = express();
+const expressLayout = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
+app.use(expressLayout);
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 /* Set routers */
 app.get('/', (req, res) => {
-    app.render('articles/index');
+    res.render('articles/index', {title: "home"});
 });
 
 app.listen(port, () => {console.log(`Server running on port ${port}`)});
