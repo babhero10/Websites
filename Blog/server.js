b37f7d970071323b5d174b8568aab8582149c866
babhero10 <abdallah.elsaid25@gmail.com>
2021-07-21 12:15:36 +0200

    Style article

--- Blog/server.js
+++ Blog/server.js
@@ -14,8 +14,33 @@ app.use('/statics', express.static('statics'));
 
 /* Set routers */
 app.get('/', (req, res) => {
-    article = [{link: 'https://google.com', name: 'Google'}];
-    res.render('articles/index', {title: "home", lastArticles:article});
+    lastArticle = [{link: 'https://google.com', name: 'Google'},{link: 'https://facebook.com', name: 'Facebook'}];
+    articles = [{
+            title:"post one",
+            author:"author one",
+            createAt:new Date(),
+            description:"des one"
+        },
+        {
+            title:"post two",
+            author:"author two",
+            createAt:new Date(),
+            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At expedita odit nam voluptas ducimus animi tenetur? Similique quis unde non at culpa amet ipsam porro consequatur et, illum blanditiis nihil!"
+        },
+        {
+            title:"post three",
+            author:"author three",
+            createAt:new Date(),
+            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At expedita odit nam voluptas ducimus animi tenetur? Similique quis unde non at culpa amet ipsam porro consequatur et, illum blanditiis nihil!"
+        },
+        {
+            title:"post three",
+            author:"author three",
+            createAt:new Date(),
+            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At expedita odit nam voluptas ducimus animi tenetur? Similique quis unde non at culpa amet ipsam porro consequatur et, illum blanditiis nihil!"
+        }
+        ];
+    res.render('articles/index', {title: "home", lastArticles:lastArticle, articles:articles});
 });
 
 app.listen(port, () => {console.log(`Server running on port ${port}`)});
