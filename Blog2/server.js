ed57e649af42a3956158608e4cfcd7ea6591168b
babhero10 <abdallah.elsaid25@gmail.com>
2021-07-24 13:22:29 +0200

    Setup layout

--- Blog2/server.js
+++ Blog2/server.js
@@ -1,10 +1,20 @@
+/* Global variables */
 const port = 5000;
 
+/* Server */
+// Express
 const express = require('express');
 const app = express();
-
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Ejs
+app.set('view engine', 'ejs');
+
+// Layout
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
 
 app.get('/', (req, res) => {
     res.send("Hello, World!");
